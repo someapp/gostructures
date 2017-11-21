38bd6050d19da2cf4a08df6b9f57f314e47466f9
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2017-11-21 21:43:39 +0000

    Add some large example mazes
    without memoization these would take ages

--- dynamic_programming_test.go
+++ dynamic_programming_test.go
@@ -53,3 +53,62 @@ func TestRobotMaze_NotCompletable(t *testing.T) {
 		t.Error("maze should not be completable")
 	}
 }
+
+func TestRobotMaze_Large(t *testing.T) {
+	// a massive empty maze
+	// --...--
+	// .......
+	// --...--
+	maze := make([][]bool, 1000)
+
+	for i := 0; i < 1000; i++ {
+		maze[i] = make([]bool, 1000)
+		for j := 0; j < 1000; j++ {
+			maze[i][j] = true
+		}
+	}
+
+	path, ok := RobotMaze(maze)
+	if !ok {
+		t.Error("maze should be completable")
+	}
+
+	expectedPath := make([]RobotMazePoint, 0, 1999)
+	for i := 0; i < 1000; i++ {
+		expectedPath = append(expectedPath, RobotMazePoint{i, 0})
+	}
+
+	for j := 1; j < 1000; j++ {
+		expectedPath = append(expectedPath, RobotMazePoint{999, j})
+	}
+
+	if !reflect.DeepEqual(expectedPath, path) {
+		t.Errorf("unexpected path %v", path)
+	}
+}
+
+func TestRobotMaze_Large_Fail(t *testing.T) {
+	// a massive empty maze
+	// that looks solvable
+	// but isnt
+	// -X...--
+	// X-...--
+	// .......
+	// --...--
+	maze := make([][]bool, 1000)
+
+	for i := 0; i < 1000; i++ {
+		maze[i] = make([]bool, 1000)
+		for j := 0; j < 1000; j++ {
+			maze[i][j] = true
+		}
+	}
+
+	maze[1][0] = false
+	maze[0][1] = false
+
+	_, ok := RobotMaze(maze)
+	if ok {
+		t.Error("maze should not be completable")
+	}
+}
