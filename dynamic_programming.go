d5e8630df7cb23f699a11453dac3bbf75831c3b3
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2017-11-21 21:08:10 +0000

    Cache misses

--- dynamic_programming.go
+++ dynamic_programming.go
@@ -4,10 +4,11 @@ package gostructures
 // can the robot navigate from the top left to bottom right
 // and if so return a possible path
 func RobotMaze(maze [][]bool) ([]RobotMazePoint, bool) {
-	return robotMazePath(maze, len(maze)-1, len(maze[0])-1)
+	cache := make(map[[2]int]bool)
+	return robotMazePath(maze, len(maze)-1, len(maze[0])-1, &cache)
 }
 
-func robotMazePath(maze [][]bool, row int, col int) ([]RobotMazePoint, bool) {
+func robotMazePath(maze [][]bool, row int, col int, cache *map[[2]int]bool) ([]RobotMazePoint, bool) {
 	if row == 0 && col == 0 {
 		return []RobotMazePoint{{x: col, y: row}}, true
 	}
@@ -16,16 +17,22 @@ func robotMazePath(maze [][]bool, row int, col int) ([]RobotMazePoint, bool) {
 		return make([]RobotMazePoint, 0), false
 	}
 
-	if path, ok := robotMazePath(maze, row-1, col); ok {
+	cached, ok := (*cache)[[2]int{col, row}]
+	if ok && !cached {
+		return make([]RobotMazePoint, 0), false
+	}
+
+	if path, ok := robotMazePath(maze, row-1, col, cache); ok {
 		path = append(path, RobotMazePoint{x: col, y: row})
 		return path, true
 	}
 
-	if path, ok := robotMazePath(maze, row, col-1); ok {
+	if path, ok := robotMazePath(maze, row, col-1, cache); ok {
 		path = append(path, RobotMazePoint{x: col, y: row})
 		return path, true
 	}
 
+	(*cache)[[2]int{col, row}] = false
 	return make([]RobotMazePoint, 0), false
 }
 
